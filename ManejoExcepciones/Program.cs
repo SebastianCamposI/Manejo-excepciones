e660d357d346c96df9a8c9d6c2d8866b6bbea34f
SebastianCamposI <105455746+SebastianCamposI@users.noreply.github.com>
2022-05-24 11:45:34 -0700

    ExcepcionesM

---
+++ ManejoExcepciones/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoExcepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingresa un número entero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("El cuadrado de " + num1 + " es " + Math.Pow(num1, 2));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
